5519fbe5c7f96962690e8b8dc861ffa20de92185
NathanRMS1 <nathanconta01@gmail.com>
2022-06-09 08:30:31 -0300

    braços funcionando

--- projeto/projeto.ino
+++ projeto/projeto.ino
@@ -1,8 +1,12 @@
-#import <Servo.h>
+#include <Servo.h>
+#include <EEPROM.h>//used to store recorded values
 Servo motor1;
 Servo motor2;
 Servo motor3;
 Servo motor4;
+float resolution = 1000;//MUST be less than 1024
+float recordTime = 50;//MUST be less than 1024
+bool recording = false;
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0, INPUT);
@@ -11,28 +15,40 @@ void setup() {
   pinMode(2, INPUT);
   motor1.attach(3);
   pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
   motor2.attach(5);
   motor3.attach(6);
+  Serial.begin(9600);
+  digitalWrite(4, HIGH);
+  digitalWrite(7, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  motor1.write(analogRead(A0)/4);
-  motor2.write(analogRead(A1)/4);
-  motor3.write(analogRead(A2)/4);
-  if(digitalRead(2)){
-    motor4.write(255);
-  }else{
-    motor4.write(0);
-  }
+  
+  /*if (recording == true) {
+    for (int i = 1; i <= resolution; i++) {
+      int val = map(analogRead(A0), 0, 1023, 0, 180);
+      EEPROM.write(i, val);
+      //Serial.println(EEPROM.read(i));
+      motor1.write(val);
+      delay(recordTime);
+    }
+    digitalWrite(13, LOW); //turn off status led
+    delay(1000);//give time for person
+    recording = false;
+  }*/
+  Serial.println(analogRead(A0));
+  motor1.write(map(analogRead(A0), 0, 1023, 0, 180));
+  motor2.write(map(analogRead(A1), 0, 1023, 0, 180));
+  motor3.write(map(analogRead(A2), 0, 1023, 0, 180));
   delay(50);
 }
 /*
 https://create.arduino.cc/projecthub/millerman4487/simple-record-and-playback-0bffa1
 #include <Servo.h>
-#include <EEPROM.h>//used to store recorded values
 Servo myServo;
-float resolution = 1000;//MUST be less than EEPROM.length()
+
 float recordTime = 5; //delay time
 bool recording = false;
 void setup() {
